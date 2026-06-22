5b9dd0f261397f0828fd10ad1fdfc3365f9e33fa
Priyanka05f0 <lakshmipriyankabethampudi.23@gmail.com>
2026-06-22 12:34:48 +0530

    Fix: remove alias outputs, fix deprecated dynamodb_table param

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -9,15 +9,3 @@ output "function_arn" {
 output "invoke_arn" {
   value = aws_lambda_function.hello.invoke_arn
 }
-
-output "function_version" {
-  value = aws_lambda_function.hello.version
-}
-
-output "blue_alias_arn" {
-  value = var.enable_blue_green ? aws_lambda_alias.blue[0].arn : null
-}
-
-output "green_alias_arn" {
-  value = var.enable_blue_green ? aws_lambda_alias.green[0].arn : null
-}
